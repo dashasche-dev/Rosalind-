833ee45568740d6b3c7c5d7ede04a2944d84a272
dashasche <dasha.sche@gmail.com>
2026-04-30 18:16:57 +0900

    solution

---
+++ dna.py
@@ -0,0 +1,7 @@
+# Counting DNA nucleotides.
+s = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+a = s.count("A")
+c = s.count("C")
+g = s.count("G")
+t = s.count("T")
+print(f"{a} {c} {g} {t}")
